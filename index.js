fbfad6116252975afc2255b0860e8f61d4cacc94
Ruby <k773352@gmail.com>
2022-07-27 22:56:00 +0000

    index.js

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ const  http = require('http'), //This module provides the HTTP server functional
        xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to uitlise XSL Transformations
        xml2js = require('xml2js') //This module does XML <-> JSON conversion
        
+const  router = express(),
+       server = http.createServer(router);
+
+router.get('/get/html', function(req, res) {
+
+       res.writeHead(200, {'Content-Type' : 'text/html'});
+       
+       let xml = fs.readFileSync('Topjobs.xml', 'utf8'),
+           xsl = fs.readFileSync('Topjobs.xsl', 'utf8');
+
+       let doc = xmlParse(xml),
+           stylesheet = xmlParse(xsl);
+
+       let result = xsltProcess(doc, stylesheet);
+
+       res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+       const addr = server.address();
+       console.log("Server listening at", addr.address + ":" + addr.port)
+});
